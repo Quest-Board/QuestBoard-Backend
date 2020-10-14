66ec65bdbfec299e6beb0b1a71ca57da865f6228
njtucker <njtucker@iastate.edu>
2020-10-14 17:43:35 -0500

    MSTest

---
+++ Questboard-Backend.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuestBoard_Backend.Models.UserModels;
+
+namespace Questboard_Backend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        User user = new User();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
