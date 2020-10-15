51f54ca89aba0888b834784adb602b30205c87e7
Alexander Young <youngale@iastate.edu>
2020-10-14 22:11:37 -0500

    Reference new user model

--- QuestBoard-Backend/Context/QuestboardContext.cs
+++ QuestBoard-Backend/Context/QuestboardContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using QuestBoard.Models.UserModels;
+using QuestBoard.Models;
 
 namespace QuestBoard.Context
 {
