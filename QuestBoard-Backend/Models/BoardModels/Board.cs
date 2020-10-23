c8c1df201acb53187f151cf2c6c55c802510be0b
Alexander Young <youngale@iastate.edu>
2020-10-23 01:37:29 -0500

    Fix the namespace of the model.

--- QuestBoard-Backend/Models/BoardModels/Board.cs
+++ QuestBoard-Backend/Models/BoardModels/Board.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using QuestBoard.Models;
 
-namespace QuestBoard_Backend.Models.BoardModels
+namespace QuestBoard.Models
 {
     public class Board
     {
