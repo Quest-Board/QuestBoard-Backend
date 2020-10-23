dcc890bfe4b0754bac6473fc29a38fab75c6f58c
Alexander Young <youngale@iastate.edu>
2020-10-23 01:33:54 -0500

    Serialisation, with an S not a Zed.

--- QuestBoard-Backend/Context/QuestboardContext.cs
+++ QuestBoard-Backend/Context/QuestboardContext.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
 using QuestBoard.Models;
 using QuestBoard_Backend.Models.BoardModels;
 
@@ -11,5 +15,33 @@ namespace QuestBoard.Context
         public QuestboardContext(DbContextOptions<QuestboardContext> options) : base(options) { }
 
         public DbSet<Board> Boards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Entity<Board>()
+                .Property(b => b.Columns)
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<List<string>>(v)
+                );
+
+            builder.Entity<Board>()
+                .Property(b => b.Columns)
+                .Metadata
+                .SetValueComparer(
+                    new ValueComparer<List<string>>(
+                        (a, b) => a.SequenceEqual(b),
+                        c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                        c => c.ToList()
+                    )
+                );
+
+            base.OnModelCreating(builder);
+        }
     }
 }
