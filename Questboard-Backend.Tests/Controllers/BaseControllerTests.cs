f22c49d016a3d97c094dac69d50472ba40540ef9
Alexander Young <youngale@iastate.edu>
2020-11-06 16:27:00 -0600

    Add BoardController Tests

---
+++ Questboard-Backend.Tests/Controllers/BaseControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using QuestBoard.Context;
+using QuestBoard.Controllers;
+using QuestBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Questboard.Tests.Controllers
+{
+    public class BaseControllerTests<TController>
+        where TController : ControllerBase
+    {
+        protected static User User {
+            get
+            {
+                return new User
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Email = "test@example.com",
+                    EmailConfirmed = true,
+                    UserName = "test@example.com",
+                    TwoFactorEnabled = false,
+                };
+            }
+            set { }
+        }
+
+        protected static readonly ClaimsPrincipal identity = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, User.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, User.Id),
+                },
+                "test"
+            )
+        );
+
+        public static QuestboardContext MakeContext()
+        {
+            DbContextOptions<QuestboardContext> options = new DbContextOptionsBuilder<QuestboardContext>()
+                .UseInMemoryDatabase(databaseName: "quest")
+                .Options;
+
+            return new QuestboardContext(options);
+        }
+
+        public static Mock<UserManager<TIDentityUser>> GetUserManagerMock<TIDentityUser>() where TIDentityUser : IdentityUser
+        {
+            return new Mock<UserManager<TIDentityUser>>(
+                new Mock<IUserStore<TIDentityUser>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<TIDentityUser>>().Object,
+                new IUserValidator<TIDentityUser>[0],
+                new IPasswordValidator<TIDentityUser>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<TIDentityUser>>>().Object
+            );
+        }
+
+        public static UserManager<User> MakeUserManager()
+        {
+            Mock<UserManager<User>> mockUserManager = GetUserManagerMock<User>();
+
+            mockUserManager.Setup(u => u.FindByIdAsync(It.IsAny<string>())).Returns(Task.FromResult(User));
+            mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).Returns(Task.FromResult(User));
+            mockUserManager.Setup(u => u.FindByEmailAsync(It.IsAny<string>())).Returns(Task.FromResult(User));
+
+            return mockUserManager.Object;
+        }
+
+        public static ILogger<TController> MakeLogger() => new Mock<ILogger<TController>>().Object;
+    }
+}
---
+++ Questboard-Backend.Tests/Controllers/BoardControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuestBoard.Context;
+using QuestBoard.Controllers;
+using QuestBoard.Models;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Questboard.Tests.Controllers
+{
+    [TestClass]
+    public class BoardControllerTests : BaseControllerTests<BoardController>
+    {
+        private static readonly QuestboardContext context = MakeContext();
+
+        private BoardController MakeController()
+        {
+            BoardController controller = new BoardController(context, MakeUserManager(), MakeLogger())
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        User = identity,
+                    }
+                }
+            };
+
+            return controller;
+        }
+
+        [TestMethod]
+        public async Task TestBoardGetsCreated()
+        {
+            BoardController controller = MakeController();
+
+            IActionResult result = await controller.CreateAsync("Test");
+            OkObjectResult okResult = result as OkObjectResult;
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.IsInstanceOfType(okResult.Value, typeof(object));
+
+            dynamic response = new DynamicObjectResultValue(okResult.Value);
+
+            Assert.IsNotNull(response);
+
+            Assert.IsTrue(response.id is int);
+
+            Board board = context.Boards.Find(response.id);
+            Assert.AreEqual(board.BoardName, "Test");
+        }
+
+        [TestMethod]
+        public async Task TestBoardAddsMember()
+        {
+            BoardController controller = MakeController();
+
+            IActionResult result = await controller.CreateAsync("TestAdd");
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            dynamic response = new DynamicObjectResultValue((result as OkObjectResult).Value);
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.id is int);
+
+            result = await controller.AddMember(response.id, new BoardController.MemberEmail
+            {
+                Email = User.Email,
+            });
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+            response = new DynamicObjectResultValue((result as OkObjectResult).Value);
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Success is bool);
+            Assert.IsTrue(response.Success);
+        }
+    }
+}
