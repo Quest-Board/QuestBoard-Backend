3cd07c355ed3d445b8808b2440aef4144d49db11
njtucker <njtucker@iastate.edu>
2020-11-11 18:48:50 -0600

    add and remove points from user

---
+++ QuestBoard-Backend/Controllers/PointsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuestBoard.Context;
+using QuestBoard.Models;
+
+namespace QuestBoard_Backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class PointsController : Controller
+    {
+        private readonly QuestboardContext _context;
+
+        private readonly UserManager<User> _userManager;
+
+        public PointsController(QuestboardContext context, UserManager<User> userManager)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public class PointsModification
+        {
+            [Required]
+            public int Points { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddAsync([FromBody]PointsModification modification)
+        {
+            User user = await _userManager.FindByEmailAsync(modification.Email);
+
+            if (user == null)
+            {
+                return NotFound("No user with that email exists");
+            }
+
+            user.Points += modification.Points;
+            _context.SaveChanges();
+
+            return Ok(new { Success = true });
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> RemoveAsync([FromBody] PointsModification modification)
+        {
+            User user = await _userManager.FindByEmailAsync(modification.Email);
+
+            if (user == null)
+            {
+                return NotFound("No user with that email exists");
+            }
+
+            user.Points += modification.Points;
+            _context.SaveChanges();
+
+            return Ok(new { Success = true });
+        }
+    }
+}
--- QuestBoard-Backend/Models/UserModels/User.cs
+++ QuestBoard-Backend/Models/UserModels/User.cs
@@ -6,5 +6,6 @@ namespace QuestBoard.Models
     public class User : IdentityUser
     {
         public ICollection<MemberOf> Boards { get; set; }
+        public int Points { get; set; }
     }
 }
